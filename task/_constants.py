a734095eee2a27d23cf2fd6cd66164531a3aadf6
Edward Chopuryan <Edward_Chopuryan@epam.com>
2026-02-02 18:55:30 -0500

    adds env

--- task/_constants.py
+++ task/_constants.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+# Load variables from .env file into environment
+load_dotenv()
 
 DIAL_URL = 'https://ai-proxy.lab.epam.com'
 API_KEY = os.getenv('DIAL_API_KEY', '')
